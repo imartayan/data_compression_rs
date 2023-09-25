25fe61e136a7febeccc8ecc563c0eec93372f895
Igor Martayan <igor@martayan.org>
2023-09-25 22:58:24 +0200

    Implement msb and lsb

--- src/introduction/util.rs
+++ src/introduction/util.rs
@@ -1,4 +1,17 @@
+use num_traits::int::PrimInt;
+
 /// number of 64-bit words for num_bits
 pub fn num_64bit_words_for(num_bits: usize) -> usize {
     (num_bits + 64 - 1) / 64
+}
+
+/// position of the most significant bit (msb)
+pub fn msb<T: PrimInt>(x: T) -> usize {
+    let num_bits = T::zero().count_zeros();
+    (num_bits - 1 - x.leading_zeros()) as usize
+}
+
+/// position of the least significant bit (lsb)
+pub fn lsb<T: PrimInt>(x: T) -> usize {
+    x.trailing_zeros() as usize
 }
